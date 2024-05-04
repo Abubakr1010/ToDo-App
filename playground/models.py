de45be6e68382b36faa59c3c4d3040c0dcc40a37
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2024-05-04 23:25:47 +0200

    my models

--- playground/models.py
+++ playground/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 # Create your models here.
-class Profile(models.Models):
-    user_id = models.IntegerField(validators=[MinValueValidator(1)])
+class User(models.Models):
+    user_id = models.IntegerField(primary_key=True,validators=[MinValueValidator(1)])
     first_name = models.CharField(max_lenght=30)
     last_name = models.CharField(max_lenght=30)
     email = models.EmailField(max_lenght=100)
     password = models.CharField(max_lenght=32)
     image = models.ImageField(upload_to='profile_images/',blank=True, null=True)
+
+class Task(models.Models):
+    task_id = models.CharField(primary_key=True,validators=[MinValueValidator(1)])
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    text = models.CharField(max_lenght=256)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+
+
+
 
